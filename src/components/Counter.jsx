347c98b4cd0cb99f36d44121f0ade45dc70ddb86
AlexandreDev <eztalenda@gmail.com>
2022-05-31 02:26:58 -0300

    feat(main): imutability context implement when we need use

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,6 +2,16 @@ import { useState } from 'react'
 
 //hook -> gancho
 
+//imutabilidade
+
+// usuarios = ['diego3g', 'diegosf', 'danileao']
+
+// usuarios.push('rafacamarda')
+
+//No estado do react quando queremos adicionar uma nova info no array, a gente sempre vai criar um novo
+
+// novoUsuarios = [...usuarios, 'rafacamarda']
+
 export function Counter() {
   //const x let
   const [counter, setCounter] = useState(0)
